85b92e530c6f596c5b4fac901554062ef2f1a8a4
Langat20 <kipkuruie7.lang@gmail.com>
2022-06-10 22:29:18 +0300

    create model class profile

--- Awards/settings.py
+++ Awards/settings.py
@@ -75,11 +75,12 @@ WSGI_APPLICATION = 'Awards.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'award',
+        'USER': 'moringa',
+        'PASSWORD': 'langat20',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
