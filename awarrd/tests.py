c183f301972d7e5568075674d4e7547b57394ce8
Langat20 <kipkuruie7.lang@gmail.com>
2022-06-10 21:53:35 +0300

    create test to test the profile

--- awarrd/tests.py
+++ awarrd/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from .models import *
 
-# Create your tests here.
+
+class TestProfile(TestCase):
+    def setUp(self):
+        self.user = User(id=1, username='dan', password='njoro1234')
+        self.user.save()
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.user, User))
+
+    def test_save_user(self):
+        self.user.save()
+
+    def test_delete_user(self):
+        self.user.delete()
+
+
+
+
