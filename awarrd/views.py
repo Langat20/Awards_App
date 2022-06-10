584431232fc36d235c3ff65b55f7ff98dba3b966
Langat20 <kipkuruie7.lang@gmail.com>
2022-06-10 22:35:06 +0300

    create view sign up

--- awarrd/views.py
+++ awarrd/views.py
@@ -32,3 +32,17 @@ def index(request):
         posts = None
     return render(request, 'index.html', {'posts': posts, 'form': form,})
 
+def signup(request):
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('index')
+    else:
+        form = SignupForm()
+    return render(request, 'registration/signup.html', {'form': form})
+
