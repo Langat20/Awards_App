555ae13ac39a2a894d3b1f0ec5d3f0788eee0aa1
Langat20 <kipkuruie7.lang@gmail.com>
2022-06-10 22:38:51 +0300

    create view edit profile

--- awarrd/views.py
+++ awarrd/views.py
@@ -120,3 +120,21 @@ def user_profile(request, username):
     }
     return render(request, 'userprofile.html', params)
 
+@login_required(login_url='login')
+def edit_profile(request, username):
+    user = User.objects.get(username=username)
+    if request.method == 'POST':
+        user_form = UserForm(request.POST, instance=request.user)
+        prof_form = ProfileForm(request.POST, request.FILES, instance=request.user.profile)
+        if user_form.is_valid() and prof_form.is_valid():
+            user_form.save()
+            prof_form.save()
+            return redirect('profile', user.username)
+    else:
+        user_form = UserForm(instance=request.user)
+        prof_form = ProfileForm(instance=request.user.profile)
+    params = {
+        'user_form': user_form,
+        'prof_form': prof_form
+    }
+    return render(request, 'edit.html', params)
