cb9feebdcef69b638df0d5ca74cb1ec548aacf9f
Langat20 <kipkuruie7.lang@gmail.com>
2022-06-10 22:37:52 +0300

    create view view profile

--- awarrd/views.py
+++ awarrd/views.py
@@ -111,3 +111,12 @@ def profile(request, username):
     return render(request, 'profile.html')
 
 
+def user_profile(request, username):
+    user_prof = get_object_or_404(User, username=username)
+    if request.user == user_prof:
+        return redirect('profile', username=request.user.username)
+    params = {
+        'user_prof': user_prof,
+    }
+    return render(request, 'userprofile.html', params)
+
