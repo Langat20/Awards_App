6356a991ee7778aac69f47ed002e7a2495033ed5
Langat20 <kipkuruie7.lang@gmail.com>
2022-06-10 22:59:22 +0300

    add install apps

--- Awards/settings.py
+++ Awards/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'awarrd',
+    'cripspy_forms',
+    'boostrap4',
 ]
 
 MIDDLEWARE = [
