4cc8847d7b965aa3eb2b9c764c81ec970267f76a
Langat20 <kipkuruie7.lang@gmail.com>
2022-06-10 22:31:01 +0300

    create model class post

--- awarrd/models.py
+++ awarrd/models.py
@@ -24,4 +24,28 @@ class Profile(models.Model):
     def save_user_profile(sender, instance, **kwargs):
         instance.profile.save()
 
+class Post(models.Model):
+    title = models.CharField(max_length=155)
+    url = models.URLField(max_length=255)
+    description = models.TextField(max_length=255)
+    photo = ImageField(manual_crop='1280x720')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
+    date = models.DateTimeField(auto_now_add=True, blank=True)
+
+    def __str__(self):
+        return f'{self.title}'
+
+    def delete_post(self):
+        self.delete()
+
+    @classmethod
+    def search_project(cls, title):
+        return cls.objects.filter(title__icontains=title).all()
+
+    @classmethod
+    def all_posts(cls):
+        return cls.objects.all()
+
+    def save_post(self):
+        self.save()
 
