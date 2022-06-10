90edb477d7f931865209094f228f969c5a3f4bdf
Langat20 <kipkuruie7.lang@gmail.com>
2022-06-10 22:34:26 +0300

    create view index

--- awarrd/views.py
+++ awarrd/views.py
@@ -12,3 +12,23 @@ from django.contrib.auth.models import User
 def welcome(request):
     return render(request, 'welcome.html')
 
+def index(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.user = request.user
+            post.save()
+    else:
+        form = PostForm()
+
+    try:
+        posts = Post.objects.all()
+        posts = posts[::-1]
+        # a_post = random.randint(0, len(posts)-1)
+        # random_post = posts[a_post]
+        # print(random_post.photo)
+    except Post.DoesNotExist:
+        posts = None
+    return render(request, 'index.html', {'posts': posts, 'form': form,})
+
