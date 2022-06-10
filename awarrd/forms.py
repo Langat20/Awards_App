9381c600e85c5113ed180a93cbcb8c3421b740b3
Langat20 <kipkuruie7.lang@gmail.com>
2022-06-10 22:43:41 +0300

    include a postform

--- awarrd/forms.py
+++ awarrd/forms.py
@@ -11,3 +11,10 @@ class SignupForm(UserCreationForm):
         model = User
         fields = ('username', 'email', 'password1', 'password2')
 
+class PostForm(forms.ModelForm):
+    photo = ImageField(label='')
+
+    class Meta:
+        model = Post
+        fields = ('photo', 'title', 'url', 'description',)
+
